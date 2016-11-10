46e087837167a6788f8b8f81889dd40751ac83ce
Colleen Feola <colleenfeola@Colleens-MacBook-Pro.local>
2016-11-10 10:19:33 -0500

    Condensed code to make it more efficient - complete

--- bshw3.py
+++ bshw3.py
@@ -15,37 +15,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
 # Connects to BSI admissions webpage 
+base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, 'lxml')
+
 # Converts the contents of the soup variable to a string in order to use string methods on it
 string = str(soup)
-new_string = ''
-new_string1 = ''
 
-# Iterates through all of image tags on the page and replaces the local images with the images provided by Colleen 
-for logo in soup.find_all('img'):
-	# Finds the local header image
-	if str(logo.get('src')) == '/sites/default/themes/umsi/imgs/logo.png':
-		new_string = string.replace(str(logo.get('src')), 'https://github.com/cvanlent/SI206/blob/master/HW3-StudentCopy/media/logo.png?raw=true')
-	# finds the local footer image
-	if str(logo.get('src')) == '/sites/default/themes/umsi/imgs/logo_footer.png':
-		new_string1 = new_string.replace(str(logo.get('src')), 'https://github.com/cvanlent/SI206/blob/master/HW3-StudentCopy/media/logo.png?raw=true')
+# Replaces the local header image with the image that Colleen provided 
+new_string = string.replace('/sites/default/themes/umsi/imgs/logo.png', 'https://github.com/cvanlent/SI206/blob/master/HW3-StudentCopy/media/logo.png?raw=true')
+
+# Replaces the local footer image with the image that Colleen provided 
+new_string1 = new_string.replace('/sites/default/themes/umsi/imgs/logo_footer.png', 'https://github.com/cvanlent/SI206/blob/master/HW3-StudentCopy/media/logo.png?raw=true')
 
 # Replaces student with AMAZING student
 new_string2 = new_string1.replace('student', 'AMAZING student')
-new_string3 = ''
 
-# Iterates through the iframe tags, which represents the video on the original webpage
-for video in soup.find_all('iframe'):
-	# Replaces the contents of the source attribute for the iframe tag with my personal image
-	if str(video.get('src')) == 'https://www.youtube.com/embed/mimp_3gquc4?feature=oembed':
-		new_string3 = new_string2.replace(str(video.get('src')), 'colleen.jpg')
+# Replaces video on the original webpage with my personal image 
+new_string3 = new_string2.replace('https://www.youtube.com/embed/mimp_3gquc4?feature=oembed', 'colleen.jpg')
 
-# Creates a new HTML file 
+# Creates a new HTML file & writes the changes made to the original HTML file to the new HTML file
 f = open('BSI.html','w')
-# Writes the changes made to the original HTML file to the new HTML created
 f.write(new_string3)
 f.close()
 
